a501656fed66f37aeda25528a3bc149e2ea79b6c
Chris Ingram <chris@chrisingram.me>
2016-02-11 18:15:13 +0000

    added basic classes

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ import unittest
 
 print 'hello world'
 
+class Warehouse(object):
+	def __init__(self):
+		pass
 
+class Drone(object):
+	def __init__(self):
+		pass
+
+class Product(object):
+	def __init__(self):
+		pass
+
+class Order(object):
+	def __init__(self):
+		pass
 
 
 
