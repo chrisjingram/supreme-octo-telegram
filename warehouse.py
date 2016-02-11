dfddb39b96b0a315b85e16dd0cdf43b4c786dc56
Topher Winward <hi@topher.io>
2016-02-11 19:07:38 +0000

    Simulation can load from warehouse to drone

--- warehouse.py
+++ warehouse.py
@@ -1,16 +1,20 @@
 from collections import defaultdict
+from product import *
 
 class Warehouse(object):
 	def __init__(self,x,y,products):
 		self.x = x
 		self.y = y
-		self.products = defaultdict(int)
+		self.products = products
 
 	def add_product(self, product, amount):
 		self.products[product] += amount
 
 	def remove_product(self, product, amount):
 		self.products[product] -= amount
+
+	def get_product_amount(self, product):
+		return self.products[product];
 
 	def calculate_weight(self):
 		total = 0
@@ -23,25 +27,25 @@ import unittest
 
 class TestAll(unittest.TestCase):
 	def test_empty_drone_has_0_weight(self):
-		drone = Warehouse(0,0,0)
+		drone = Warehouse(0,0,defaultdict(int))
 		self.assertEqual(0, drone.calculate_weight())
 
 	def test_drone_has_weight_one_product(self):
-		drone = Warehouse(0,0,1)
-		drone.add_product(1,1)
+		drone = Warehouse(0,0,defaultdict(int))
+		drone.add_product(Product(0, 10),1)
 		self.assertEqual(1, drone.calculate_weight())
 
 	def test_drone_has_weight_two_products(self):
-		drone = Warehouse(0,0,1)
-		drone.add_product(1,1)
-		drone.add_product(1,1)
+		drone = Warehouse(0,0,defaultdict(int))
+		drone.add_product(Product(0, 10),1)
+		drone.add_product(Product(0, 10),1)
 		self.assertEqual(2, drone.calculate_weight())
 
 	def test_drone_loses_weight(self):
-		drone = Warehouse(0,0,1)
-		drone.add_product(1,2)
-		drone.remove_product(1,1)
+		drone = Warehouse(0,0,defaultdict(int))
+		drone.add_product(Product(0, 10),2)
+		drone.remove_product(Product(0, 10),1)
 		self.assertEqual(1, drone.calculate_weight())
 
-print "--------------------"
-unittest.main()
+if __name__ == "__main__":
+	unittest.main()
