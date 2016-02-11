2025fadefe30bff2f8890940df190d1a462b0240
Chris Ingram <chris@chrisingram.me>
2016-02-11 18:22:15 +0000

    added properties to classes|

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 print 'hello world'
 
 class Warehouse(object):
-	def __init__(self):
-		pass
+	def __init__(self,x,y,products):
+		self.x = x
+		self.y = y
+		self.products = products
 
 class Drone(object):
-	def __init__(self):
-		pass
+	def __init__(self,x,y,capacity):
+		self.x = x
+		self.y = y
+		self.capacity = capacity
 
 class Product(object):
-	def __init__(self):
-		pass
+	def __init__(self,type,weight):
+		self.type = type
+		self.weight = weight
 
 class Order(object):
-	def __init__(self):
-		pass
+	def __init__(self,x,y,products):
+		self.products = products # dictionary
+		self.x = x
+		self.y = y
 
 
 
