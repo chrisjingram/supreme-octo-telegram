4674ba8be6ab21c64d7de4aa0bb06395f83ff3ea
Chris Ingram <chris@chrisingram.me>
2016-02-11 18:53:05 +0000

    Order constructor done

--- order.py
+++ order.py
@@ -1,5 +1,19 @@
+from collections import defaultdict
 class Order(object):
 	def __init__(self,x,y,products):
 		self.products = products # dictionary
 		self.x = x
 		self.y = y
+
+
+import unittest
+
+class TestAll(unittest.TestCase):
+	def test_construct_one_product(self):
+		products = defaultdict(int)
+		products[1] = 1
+		order = Order(1,1,products)
+		self.assertEqual(1,products[1])
+
+print "--------------------"
+unittest.main()
